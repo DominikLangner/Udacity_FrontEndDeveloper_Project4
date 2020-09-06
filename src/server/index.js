b2a9e0fac1b161bbab5186b0927b09cfe89a51c7
Dominik <dominik.j.langner@gmail.com>
2020-09-06 10:41:51 +0200

    initialize new project4

---
+++ src/server/index.js
@@ -0,0 +1,32 @@
+var path = require("path");
+const express = require("express");
+const mockAPIResponse = require("./mockAPI.js");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+
+app.use(express.static("dist"));
+
+console.log(__dirname);
+
+app.get("/", function (req, res) {
+  // res.sendFile('dist/index.html')
+  res.sendFile(path.resolve("src/client/views/index.html"));
+});
+
+// designates what port the app will listen to for incoming requests
+app.listen(8080, function () {
+  console.log("Example app listening on port 8080!");
+});
+
+app.get("/test", function (req, res) {
+  res.send(mockAPIResponse);
+});
+
+// Aylien API:
+var textapi = new aylien({
+  application_id: "your-api-id",
+  application_key: process.env.API_KEY,
+});
+console.log(`Your API key is ${process.env.API_KEY}`);
